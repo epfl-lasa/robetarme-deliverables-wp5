89cd0d73118c07871dd0479cf2f8e698250516cc
bonato47 <tristan_bonato@hotmail.com>
2024-03-06 12:05:50 +0100

    add package

--- src/wp5-main-temp/src/main_temp.cpp
+++ src/wp5-main-temp/src/main_temp.cpp
@@ -19,13 +19,18 @@ int main(int argc, char **argv)
     RoboticArmUr5 ur5Arm;
 
 
+
     tuple<vector<double>, vector<double>, vector<double>> stateJoints;
     while (ros::ok()){
         stateJoints = rosInterface.receive_state();
+
         vector<double>& retrievedPosition = get<0>(stateJoints);
         vector<double>& retrievedSpeed    = get<1>(stateJoints);
         vector<double>& retrievedTorque   = get<2>(stateJoints);
         std::cout << "retrievedPosition:"<<retrievedPosition[3]<< std::endl;
+        vector<double> posCart = ur5Arm.getFK(retrievedPosition);
+        std::cout << "posCart:"<<posCart[3]<< std::endl;
+
         ros::spinOnce(); // Allow the message to be subscribed
         loop_rate.sleep(); 
     }
