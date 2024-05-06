bd27df5db8fbd8799cb1c0a9cb3dc268e7d60468
bonato47 <61695400+bonato47@users.noreply.github.com>
2024-05-06 16:34:08 +0200

    Update TargetExtraction.h

--- src/wp5-planner-ros/include/TargetExtraction.h
+++ src/wp5-planner-ros/include/TargetExtraction.h
@@ -1,3 +1,10 @@
+/**
+ * @file TargetExtraction.h
+ * @brief Declaration of the TargetExtraction class
+ * @author [Author Name]
+ * @date [Date]
+ */
+
 #pragma once
 
 #include <eigen3/Eigen/Dense>
@@ -6,23 +13,60 @@
 #include <string>
 #include <vector>
 
-// this class extrac the polygon and the Pose center of the Target
-// it should be replace by the same extraction but from a CAD
+/**
+ * @brief Class for extracting information about the target.
+ *
+ * This class extracts information about the target, including its polygonal shape and center pose.
+ * It is designed to be replaced by a similar extraction method from a CAD model in future implementations.
+ */
 class TargetExtraction {
 public:
+  /**
+   * @brief Constructor for TargetExtraction.
+   *
+   * @param nh Reference to the ROS node handle.
+   */
   TargetExtraction(ros::NodeHandle& nh);
+
+  /**
+   * @brief Get the polygon vertices of the target.
+   *
+   * @return Vector of Eigen::Vector3d representing the vertices of the target polygon.
+   */
   std::vector<Eigen::Vector3d> getPolygons();
+
+  /**
+   * @brief Get the quaternion representing the orientation of the target.
+   *
+   * @return Quaternion representing the orientation of the target.
+   */
   Eigen::Quaterniond getQuatTarget();
+
+  /**
+   * @brief Get the position of the target.
+   *
+   * @return Vector representing the position of the target.
+   */
   Eigen::Vector3d getPosTarget();
+
+  /**
+   * @brief Callback function for receiving the target pose.
+   *
+   * @param msg Pointer to the received geometry_msgs::PoseStamped message.
+   */
   void CCVrpnTarget(const geometry_msgs::PoseStamped::ConstPtr msg);
+
+  /**
+   * @brief Method to visualize the target with ROS1.
+   */
   void seeTarget();
 
 private:
-  bool targetReceived_ = false;
-  double heightTarget_, widthTarget_;
-  Eigen::Quaterniond targetQuat_;
-  Eigen::Vector3d targetPos_;
-  std::vector<Eigen::Vector3d> polygonsPositions_;
-  ros::Subscriber poseTargetSub_;
-  ros::Publisher originalPolygonPub_;
+  bool targetReceived_ = false; ///< Flag indicating if the target pose has been received.
+  double heightTarget_, widthTarget_; ///< Height and width of the target.
+  Eigen::Quaterniond targetQuat_; ///< Quaternion representing the orientation of the target.
+  Eigen::Vector3d targetPos_; ///< Vector representing the position of the target.
+  std::vector<Eigen::Vector3d> polygonsPositions_; ///< Vertices of the target polygon.
+  ros::Subscriber poseTargetSub_; ///< ROS subscriber for receiving the target pose.
+  ros::Publisher originalPolygonPub_; ///< ROS publisher for publishing the original polygon.
 };
